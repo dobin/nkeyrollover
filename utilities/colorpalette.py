90ac50f4ab6adfba42c8c25e26f0e8f0059e265a
dobin <dobin@broken.ch>
2019-08-14 20:24:41 +0200

    better colors, again

--- utilities/colorpalette.py
+++ utilities/colorpalette.py
@@ -10,13 +10,26 @@ logger = logging.getLogger(__name__)
 
 
 class Color(IntEnum): 
-    green = 1
-    magenta = 2
-    red = 3
-    yellow = 4
-    blue = 5
-    cyan = 6
-    white = 7
+    brightwhite = 1
+    white = 2
+    grey = 3
+    black = 4
+
+    brightblue = 5
+    blue = 6
+    brightcyan = 7
+    cyan = 8
+
+    brightyellow = 9
+    yellow = 10
+
+    brightred = 11
+    red = 12
+    brightmagenta = 13
+    magenta = 14
+
+    brightgreen = 15
+    green = 16
 
 
 class ColorPalette(object):
@@ -44,7 +57,46 @@ class ColorPalette(object):
 
     @staticmethod
     def getColorByColor(color :Color):
-        return curses.color_pair(int(color))
+        if color is Color.brightwhite: 
+            return curses.color_pair(7) | curses.A_BOLD
+        elif color is Color.white: 
+            return curses.color_pair(7)
+        elif color is Color.grey: 
+            return curses.color_pair(8) | curses.A_BOLD
+        elif color is Color.black: 
+            return curses.color_pair(8)
+
+        elif color is Color.brightblue: 
+            return curses.color_pair(5) | curses.A_BOLD
+        elif color is Color.blue: 
+            return curses.color_pair(5)
+        elif color is Color.brightcyan: 
+            return curses.color_pair(6) | curses.A_BOLD
+        elif color is Color.cyan: 
+            return curses.color_pair(6)
+
+        elif color is Color.brightyellow: 
+            return curses.color_pair(4) | curses.A_BOLD
+        elif color is Color.yellow: 
+            return curses.color_pair(4)
+
+        elif color is Color.brightred: 
+            return curses.color_pair(3) | curses.A_BOLD
+        elif color is Color.red: 
+            return curses.color_pair(3)
+        elif color is Color.brightmagenta: 
+            return curses.color_pair(2) | curses.A_BOLD
+        elif color is Color.magenta: 
+            return curses.color_pair(2)
+
+        elif color is Color.brightgreen: 
+            return curses.color_pair(1) | curses.A_BOLD
+        elif color is Color.green: 
+            return curses.color_pair(1)
+
+        else: 
+            logging.error("Unknown color: " + str(color))
+            return curses.color_pair(1)
 
 
     @staticmethod
@@ -58,19 +110,26 @@ class ColorPalette(object):
             return color
 
         if colorType is ColorType.particle:
-            color = curses.color_pair(7)
+            color = ColorPalette.getColorByColor(Color.brightmagenta)
+        
         elif colorType is ColorType.sprite: # only init?
-            color = curses.color_pair(1)
+            color = ColorPalette.getColorByColor(Color.green)
+        
         elif colorType is ColorType.specktexture:
-            color = curses.color_pair(8) | curses.A_BOLD 
+            color = ColorPalette.getColorByColor(Color.grey)
+        
         elif colorType is ColorType.worldmap:
-            color = curses.color_pair(7)
+            color = ColorPalette.getColorByColor(Color.grey)
+        
         elif colorType is ColorType.scene:
-            color = curses.color_pair(7)
+            color = ColorPalette.getColorByColor(Color.white)
+        
         elif colorType is ColorType.world:
-            color = curses.color_pair(7)
+            color = ColorPalette.getColorByColor(Color.grey)
+        
         elif colorType is ColorType.menu:
-            color = curses.color_pair(7) | curses.A_BOLD 
+            color = ColorPalette.getColorByColor(Color.white)
+        
         else: 
             logging.error("Unknown colortype " + str(colorType))
 
