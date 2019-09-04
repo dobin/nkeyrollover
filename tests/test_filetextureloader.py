4ddc550e8f80446b863ee7f85efb724cbf9cfde0
dobin <dobin@broken.ch>
2019-09-04 09:32:22 +0200

    remove all previous unit tests, as too many things have changed

---
+++ tests/test_filetextureloader.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import unittest
+import time
+import logging
+import curses
+
+
+class FileTextureLoaderTest(unittest.TestCase):
+    def test_loadTexture(self): 
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
