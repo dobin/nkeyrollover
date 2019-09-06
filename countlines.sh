47ac14e9d76d0295eedb739c841373df72c5cecf
dobin <dobin@broken.ch>
2019-09-06 11:46:47 +0200

    dont count temp stuff

--- countlines.sh
+++ countlines.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/.local/bin/pygount --suffix "py" | awk '{ print $1}' > temp/linecount.txt
+~/.local/bin/pygount --folders-to-skip temp,ai --suffix "py" | awk '{ print $1}' > temp/linecount.txt
 awk '{s+=$1} END {print s}' temp/linecount.txt
