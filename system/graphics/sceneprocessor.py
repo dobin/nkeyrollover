06ffde4f053514992d7abaa7795a1bfd0211b65e
dobin <dobin@broken.ch>
2019-09-10 20:08:55 +0200

    move screen automatically according to scene state

--- system/graphics/sceneprocessor.py
+++ system/graphics/sceneprocessor.py
@@ -6,6 +6,7 @@ from messaging import messaging, MessageType
 from config import Config
 import system.gamelogic.ai
 import system.graphics.renderable
+from utilities.timer import Timer
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +25,8 @@ class SceneProcessor(esper.Processor):
         self.sceneManager.initScene()  # start first scene
         self.xCenter = Config.columns / 2 - 5
         self.state = State.brawl
+        self.screenMoveTimer = Timer(0.1)
+        self.lastKnownPlayerPos = None
 
 
     def numEnemiesAlive(self) -> int:
@@ -54,13 +57,19 @@ class SceneProcessor(esper.Processor):
 
 
     def process(self, dt):
+        self.screenMoveTimer.advance(dt)
+
         for message in messaging.getByType(MessageType.EntityDead):
             # if no enemies are alive, we want to go to the next akt
             if self.numEnemiesAlive() == 0:
+                logging.warning("START TIMER")
+                self.screenMoveTimer.start()
                 self.setState(State.pushToNextScene)
             break
 
         for message in messaging.getByType(MessageType.PlayerLocation):
+            self.lastKnownPlayerPos = message.data
+
             if self.state is State.pushToNextScene:
                 # if suddenly enemies appear, let the player free
                 if self.numEnemiesVisible() > 0:
@@ -68,6 +77,8 @@ class SceneProcessor(esper.Processor):
 
             if self.state is State.brawl:
                 if self.numEnemiesVisible() == 0:
+                    logging.warning("START TIMER")
+                    self.screenMoveTimer.start()
                     self.setState(State.pushToEnemies)
 
             if self.state is State.pushToEnemies:
@@ -77,6 +88,7 @@ class SceneProcessor(esper.Processor):
             playerScreenCoords = self.viewport.getScreenCoords(
                 message.data)
 
+            # adjust viewport on move
             if self.state is State.pushToNextScene:
                 # screen follows player
                 # player is left side of screen (screen pushes player right)
@@ -102,10 +114,46 @@ class SceneProcessor(esper.Processor):
                 if playerScreenCoords.x <= Config.moveBorderLeft:
                     distance = playerScreenCoords.x - Config.moveBorderLeft
                     self.viewport.adjustViewport(distance)
+            # /adjust viewport on move
 
             self.sceneManager.handlePosition(message.data, self.viewport.getx())
 
         for message in messaging.getByType(MessageType.PlayerKeypress):
             self.sceneManager.handlePlayerKeyPress()
+
+        # move screen
+        if self.screenMoveTimer.timeIsUp() and self.lastKnownPlayerPos is not None:
+            playerScreenCoords = self.viewport.getScreenCoords(
+                self.lastKnownPlayerPos)
+
+            if self.state is State.pushToNextScene:
+                # screen follows player
+                # player is left side of screen (screen pushes player right)
+                if playerScreenCoords.x != 10:
+                    distance = int(playerScreenCoords.x - 10)
+                    if distance < 0:
+                        self.viewport.adjustViewport(-1)
+                        self.screenMoveTimer.reset()
+                    elif distance > 0:
+                        self.viewport.adjustViewport(1)
+                        self.screenMoveTimer.reset()
+
+                else:
+                    self.screenMoveTimer.stop()
+
+            elif self.state is State.pushToEnemies:
+                # screen follows player
+                # player is middle of the screen
+                if playerScreenCoords.x != self.xCenter:
+                    distance = int(playerScreenCoords.x - self.xCenter)
+                    if distance < 0:
+                        self.viewport.adjustViewport(-1)
+                        self.screenMoveTimer.reset()
+                    elif distance > 0:
+                        self.viewport.adjustViewport(1)
+                        self.screenMoveTimer.reset()
+                else:
+                    self.screenMoveTimer.stop()
+
 
         self.sceneManager.advance(dt)
