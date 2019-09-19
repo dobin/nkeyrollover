a63c42343542bd851d8b25cf40a78b8e7f41b338
dobin <dobin@broken.ch>
2019-09-19 11:44:56 +0200

    split particleemiter from particleprocessor

--- system/graphics/particleprocessor.py
+++ system/graphics/particleprocessor.py
@@ -1,10 +1,6 @@
 import logging
 import esper
 
-from config import Config
-from common.direction import Direction
-from system.graphics.particle import Particle
-from system.graphics.particleeffecttype import ParticleEffectType
 from game.viewport import Viewport
 from messaging import messaging, MessageType
 from directmessaging import directMessaging, DirectMessageType
@@ -13,6 +9,7 @@ import system.gamelogic.enemy
 import system.gamelogic.player
 import system.graphics.renderable
 import system.groupid
+from system.singletons.particleemiter import ParticleEmiter
 
 logger = logging.getLogger(__name__)
 
@@ -25,12 +22,7 @@ class ParticleProcessor(esper.Processor):
         viewport :Viewport
     ):
         self.viewport :Viewport = viewport
-        self.particlePool = []
-        self.particleActive = []
-        n = 0
-        while n < Config.maxParticles:
-            self.particlePool.append(Particle(viewport=viewport))
-            n += 1
+        self.particleEmiter = ParticleEmiter(viewport=viewport)
 
 
     def process(self, dt):
@@ -41,7 +33,7 @@ class ParticleProcessor(esper.Processor):
 
     def checkMessages(self):
         for message in messaging.getByType(MessageType.EmitParticleEffect):
-            hitLocations = self.emit(
+            hitLocations = self.particleEmiter.emit(
                 loc=message.data['location'],
                 effectType=message.data['effectType'],
                 direction=message.data['direction'],
@@ -71,100 +63,13 @@ class ParticleProcessor(esper.Processor):
 
 
     def advance(self, dt):
-        for particle in self.particleActive:
+        for particle in self.particleEmiter.particleActive:
             particle.advance(dt)
 
             if not particle.isActive():
-                self.particleActive.remove(particle)
-                self.particlePool.append(particle)
+                self.particleEmiter.unuse(particle)
 
 
     def render(self):
-        for particle in self.particleActive:
+        for particle in self.particleEmiter.particleActive:
             particle.draw()
-
-
-    def emit(
-        self, loc, effectType :ParticleEffectType,
-        direction :Direction = Direction.none
-    ):
-        particleList = []
-        if effectType is ParticleEffectType.explosion:
-            particleCount = 16
-            life = 40
-            n = 0
-            while n < particleCount:
-                particle = self.particlePool.pop()
-                angle = (360.0 / particleCount) * n
-
-                particle.init(
-                    x=loc.x, y=loc.y, life=life, angle=angle,
-                    speed=0.1, fadeout=True, byStep=False, charType=0,
-                    active=True)
-
-                self.particleActive.append(particle)
-                particleList.append(particle)
-                n += 1
-
-        if effectType is ParticleEffectType.laser:
-            particleCount = 16
-            life = 60
-            n = 0
-            while n < particleCount:
-                particle = self.particlePool.pop()
-                if direction is Direction.right:
-                    angle = 0.0
-                    xinv = 1
-                else:
-                    angle = 180
-                    xinv = -1
-
-                basex = loc.x + (xinv * 6)  # distance from char
-                particle.init(
-                    x=basex + n * xinv, y=loc.y, life=life, angle=angle,
-                    speed=0.0, fadeout=True, byStep=False, charType=0,
-                    active=True)
-
-                self.particleActive.append(particle)
-                particleList.append(particle)
-                n += 1
-
-        if effectType is ParticleEffectType.cleave:
-            particleCount = 7
-            life = 60
-            n = 0
-
-            if direction is Direction.right:
-                xinv = 2
-            else:
-                xinv = -1
-
-            # for debug, source
-            # particle = self.particlePool.pop()
-            # particle.init(
-            #         x=loc.x, y=loc.y, life=life, angle=0,
-            #         speed=0.0, fadeout=True, byStep=False, charType=0,
-            #         active=True)
-            # particleList.append(particle)
-            # self.particleActive.append(particle)
-
-            while n < particleCount:
-                particle = self.particlePool.pop()
-
-                basex = loc.x + xinv  # distance from char
-                logger.debug("New particle at: {}/{}".format(basex + xinv, loc.y + n))
-                particle.init(
-                    x=basex + xinv, y=loc.y + n - int(particleCount / 2) + 1,
-                    life=life,
-                    angle=0,
-                    speed=0.0,
-                    fadeout=True,
-                    byStep=False,
-                    charType=0,
-                    active=True)
-
-                self.particleActive.append(particle)
-                particleList.append(particle)
-                n += 1
-
-        return particleList
---
+++ system/singletons/particleemiter.py
@@ -0,0 +1,119 @@
+import logging
+
+from system.graphics.particleeffecttype import ParticleEffectType
+from common.direction import Direction
+from config import Config
+from system.graphics.particle import Particle
+
+logger = logging.getLogger(__name__)
+
+
+class ParticleEmiter(object):
+    """Creates and manages particles.
+
+    * Called from ParticleProcessor
+    * Has state
+    """
+    def __init__(self, viewport):
+        self.viewport = viewport
+
+        # particlePool is private, and only used in ParticleEmiter
+        self.particlePool = []
+        n = 0
+        while n < Config.maxParticles:
+            self.particlePool.append(Particle(viewport=viewport))
+            n += 1
+
+        # particleActive is being used by ParticleProcessor, its basically public
+        self.particleActive = []
+
+
+    def unuse(self, particle):
+        self.particleActive.remove(particle)
+        self.particlePool.append(particle)
+
+
+    def emit(
+        self, loc, effectType :ParticleEffectType,
+        direction :Direction = Direction.none
+    ):
+        particleList = []
+        if effectType is ParticleEffectType.explosion:
+            particleCount = 16
+            life = 40
+            n = 0
+            while n < particleCount:
+                particle = self.particlePool.pop()
+                angle = (360.0 / particleCount) * n
+
+                particle.init(
+                    x=loc.x, y=loc.y, life=life, angle=angle,
+                    speed=0.1, fadeout=True, byStep=False, charType=0,
+                    active=True)
+
+                self.particleActive.append(particle)
+                particleList.append(particle)
+                n += 1
+
+        if effectType is ParticleEffectType.laser:
+            particleCount = 16
+            life = 60
+            n = 0
+            while n < particleCount:
+                particle = self.particlePool.pop()
+                if direction is Direction.right:
+                    angle = 0.0
+                    xinv = 1
+                else:
+                    angle = 180
+                    xinv = -1
+
+                basex = loc.x + (xinv * 6)  # distance from char
+                particle.init(
+                    x=basex + n * xinv, y=loc.y, life=life, angle=angle,
+                    speed=0.0, fadeout=True, byStep=False, charType=0,
+                    active=True)
+
+                self.particleActive.append(particle)
+                particleList.append(particle)
+                n += 1
+
+        if effectType is ParticleEffectType.cleave:
+            particleCount = 7
+            life = 60
+            n = 0
+
+            if direction is Direction.right:
+                xinv = 2
+            else:
+                xinv = -1
+
+            # for debug, source
+            # particle = self.particlePool.pop()
+            # particle.init(
+            #         x=loc.x, y=loc.y, life=life, angle=0,
+            #         speed=0.0, fadeout=True, byStep=False, charType=0,
+            #         active=True)
+            # particleList.append(particle)
+            # self.particleActive.append(particle)
+
+            while n < particleCount:
+                particle = self.particlePool.pop()
+
+                basex = loc.x + xinv  # distance from char
+                logger.debug("New particle at: {}/{}".format(basex + xinv, loc.y + n))
+                particle.init(
+                    x=basex + xinv, y=loc.y + n - int(particleCount / 2) + 1,
+                    life=life,
+                    angle=0,
+                    speed=0.0,
+                    fadeout=True,
+                    byStep=False,
+                    charType=0,
+                    active=True)
+
+                self.particleActive.append(particle)
+                particleList.append(particle)
+                n += 1
+
+        return particleList
