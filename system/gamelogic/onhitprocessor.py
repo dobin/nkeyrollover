67c697442ef2ac5285c4474d7b88b4865c4d3a63
Dobin <dobin@broken.ch>
2019-12-09 22:34:38 +0100

    fix: make on-hit effects only when hitting enemy

--- system/gamelogic/onhitprocessor.py
+++ system/gamelogic/onhitprocessor.py
@@ -26,34 +26,41 @@ class OnhitProcessor(esper.Processor):
         for msg in messaging.getByType(MessageType.AttackAt):
             hitLocations = msg.data['hitLocations']
             #damage = msg.data['damage']
-            #byPlayer = msg.data['byPlayer']
+            byPlayer = msg.data['byPlayer']
             direction = msg.data['direction']
             #knockback = msg.data['knockback']
             #stun = msg.data['stun']
             
-            if 'onHitEffect' in msg.data:
-                onHitEffect = msg.data['onHitEffect']
-            else:
-                onHitEffect = None
+            #if 'onHitEffect' in msg.data:
+            #    onHitEffect = msg.data['onHitEffect']
+            #else:
+            #    onHitEffect = None
+            onHitEffect = True
 
             if onHitEffect:
-                self.handleOnHit(hitLocations, direction)
+                self.handleOnHit(hitLocations, direction, byPlayer)
 
 
-    def handleOnHit(self, hitLocations, direction):
+    def handleOnHit(self, hitLocations, direction, byPlayer):
             for entity, (meAtk, groupId, renderable) in self.world.get_components(
                 Attackable, GroupId, Renderable
             ):
+                isPlayer = self.world.has_component(entity, Player)
+                # enemy can attack player, and vice-versa
+                if not byPlayer ^ isPlayer:
+                    continue
+
                 if renderable.isHitBy(hitLocations):
                     hitLocations = renderable.getHitLocationsOf(hitLocations)
 
-                    messaging.add(
-                        type=MessageType.EmitParticleEffect,
-                        data={
-                            'location': hitLocations[0],
-                            'effectType': ParticleEffectType.impact,
-                            'damage': 0,
-                            'byPlayer': True,
-                            'direction': direction,
-                        }
-                    )    
+                    for hitLocation in hitLocations:
+                        messaging.add(
+                            type=MessageType.EmitParticleEffect,
+                            data={
+                                'location': hitLocation,
+                                'effectType': ParticleEffectType.impact,
+                                'damage': 0,
+                                'byPlayer': True,
+                                'direction': direction,
+                            }
+                        )    
